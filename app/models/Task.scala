4690a4578d2403f136b40caa003989293aa425aa
Edgar Perez Garcia <edgar-cid@hotmail.com>
2014-09-18 18:21:30 +0200

    Cambiado el estilo de ambitos

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -9,30 +9,33 @@ case class Task(id: Long, label: String)
 
 object Task
 {
-   def all(): List[Task] = DB.withConnection { implicit c =>
-      SQL("select * from task").as(task *)
+   def all(): List[Task] = DB.withConnection
+   {
+      implicit c => SQL("select * from task").as(task *)
    }
 
 
-   def create(label: String) {
-      DB.withConnection { implicit c =>
-         SQL("insert into task (label) values ({label})").on(
-            'label -> label
-         ).executeUpdate()
+   def create(label: String)
+   {
+      DB.withConnection
+      {
+         implicit c => SQL("insert into task (label) values ({label})").on('label -> label).executeUpdate()
       }
    }
 
-   def delete(id: Long) {
-      DB.withConnection { implicit c =>
-         SQL("delete from task where id = {id}").on(
-            'id -> id
-         ).executeUpdate()
+   def delete(id: Long)
+   {
+      DB.withConnection
+      {
+         implicit c => SQL("delete from task where id = {id}").on('id -> id).executeUpdate()
       }
    }
 
 
-   val task = {
-      get[Long]("id") ~ get[String]("label") map {
+   val task =
+   {
+      get[Long]("id") ~ get[String]("label") map
+      {
          case id~label => Task(id, label)
       }
    }
