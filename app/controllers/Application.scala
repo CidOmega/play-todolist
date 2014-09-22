c6b83a8b33bead815416cc1763c435b4f1301042
Edgar Perez Garcia <edgar-cid@hotmail.com>
2014-09-22 16:53:36 +0200

    GET /tasks ya es API REST
    -Adicionalmente se ha reciclado el antiguo indice en /ui
    -GET / redirecciona a /ui, es necesaria una validación de requisitos

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import play.api.libs.json._
 
 import models.Task
 
@@ -11,11 +12,17 @@ object Application extends Controller
 {
    def index = Action
    {
-      Redirect(routes.Application.tasks)
+      Redirect(routes.Application.ui)
    }
 
 
    def tasks = Action
+   {
+      Ok(Json.toJson(Task.all))
+   }
+
+
+   def ui = Action
    {
       Ok(views.html.index(Task.all(), taskForm))
    }
@@ -32,7 +39,7 @@ object Application extends Controller
             }
          )
    }
-   
+
 
    def deleteTask(id: Long) = Action
    {
