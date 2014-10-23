efc7c6a4176dbea202f412f8dd8dbed1c37e0dfd
Edgar Perez Garcia <edgar-cid@hotmail.com>
2014-10-23 14:12:09 +0200

    Agregados más tests de modelo de Task

--- test/models/TaskSpec.scala
+++ test/models/TaskSpec.scala
@@ -7,14 +7,13 @@ import play.api.test.Helpers._
 
 import models._
 import java.util.{Date}
-import java.text.{SimpleDateFormat}
+import java.sql.{Timestamp} //Esto por listillo (aunque el unico problema era el formato de salida...)
 
 @RunWith(classOf[JUnitRunner])
 class TaskSpec extends Specification {
 
    "Task" should {
 
-      val parser = new SimpleDateFormat("dd/MM/yyyy")
       var listTasks : List[Long] = Nil
 
       def before: Unit =
@@ -33,23 +32,40 @@ class TaskSpec extends Specification {
       }
 
       "Crear tareas correctamente" in new WithApplication{
-         var ret = Task.create("Tarea de prueba", "edgar", Some(parser.parse("25/11/1993")))
+         var ret = Task.create("Tarea de prueba", "edgar", Some(new Timestamp(754182000000L)))
 
          ret must beSome(be_>(-1L))
 
          addToListTasks(ret.get)
       }
 
-      "Recuperar todos los datos de la tarea" in new WithApplication() {
+      "Recuperar todos los datos de la tarea existente y None de la no existente" in new WithApplication() {
          val label = "Tarea de prueba"
          val userNick = "edgar"
-         val deadend = Some(parser.parse("25/11/1993"))
+         val deadend : Option[Date] = Some(new Timestamp(754182000000L))//754182000000L == 25/11/1993
 
          var id = Task.create(label, userNick, deadend).get
          addToListTasks(id)
 
          Task.readOption(id) must beSome(Task(id ,label, User(userNick), deadend))
+         Task.readOption(-1L) must beNone
       }
-      
+
+      "Borrar las tareas correctamente" in new WithApplication() {
+         val label = "Tarea de prueba"
+         val userNick = "edgar"
+         val deadend : Option[Date] = Some(new Timestamp(754182000000L))//754182000000L == 25/11/1993
+
+         var id = Task.create(label, userNick, deadend).get
+         //addToListTasks(id) //No necesario porque se borra en el propio test
+
+         Task.readOption(id) must beSome
+         Task.delete(id) === 1
+         Task.readOption(id) must beNone
+         Task.delete(id) === 0
+
+         Task.delete(-1L) === 0
+      }
+
    }
 }
