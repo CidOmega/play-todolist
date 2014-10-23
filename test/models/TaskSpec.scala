1ca311a2af25d7bb8075b7b14b26faf2c10902cb
Edgar Perez Garcia <edgar-cid@hotmail.com>
2014-10-23 16:49:52 +0200

    Añadido test sobre Task.tasksOfUserWithoutDeadend

--- test/models/TaskSpec.scala
+++ test/models/TaskSpec.scala
@@ -46,6 +46,31 @@ class TaskSpec extends Specification {
          Task.delete(-1L) === 0
       }
 
+      "Borrar las tareas anteriores a cierta fecha correctamente" in new WithApplication() {
+         val label = "Tarea de prueba"
+         val userNick = "edgar"
+         val fechas : Array[Option[Date]] = Array(
+            Some(new Timestamp(754182000000L)),//754182000000L == 25/11/1993
+            Some(new Timestamp(785718000000L)),//754182000000L == 25/11/1994
+            Some(new Timestamp(817254000000L)),//754182000000L == 25/11/1995
+            Some(new Timestamp(848876400000L)))//754182000000L == 25/11/1996
+
+         Task.create(label, userNick, fechas(0))
+         Task.create(label + label, userNick, fechas(1))
+         Task.create(label + label + label, userNick, fechas(2))
+         Task.create("", userNick, fechas(3))
+
+         Task.deleteTasksOfUserEndsBefore(userNick, fechas(2).get) === 2
+
+         val tasks = Task.allOfUser(userNick).toArray
+
+         tasks.length === 2
+         //No se como hacer que ignore el id
+         tasks(0) === Task(tasks(0).id, label + label + label, User(userNick), fechas(2))
+         tasks(1) === Task(tasks(1).id, "", User(userNick), fechas(3))
+         tasks(0).id !== tasks(1).id
+      }
+
       "Recuperar bien todas las tareas de un usuario" in new WithApplication() {
          val label = "Tarea de prueba"
          val userNick = "edgar"
@@ -62,6 +87,29 @@ class TaskSpec extends Specification {
          //No se como hacer que ignore el id
          tasks(0) === Task(tasks(0).id, label, User(userNick), deadend)
          tasks(1) === Task(tasks(1).id, label+label, User(userNick), deadend)
+         tasks(0).id !== tasks(1).id
+      }
+
+   }
+
+   "Task sobre filtros de deadend" should {
+
+      "Devolver solo tareas sin deadend" in new WithApplication() {
+         val label = "Tarea de prueba"
+         val userNick = "edgar"
+         val deadend : Option[Date] = Some(new Timestamp(754182000000L))//754182000000L == 25/11/1993
+
+         Task.create(label, userNick, None)
+         Task.create(label+label, userNick, None)
+         Task.create("", userNick, deadend)//Con fecha del mismo usuario, no debe recuperarse
+         Task.create(label, "domingo", None)//Sin fecha pero de otro user, no debe recuperarse
+
+         val tasks = Task.tasksOfUserWithoutDeadend(userNick).toArray
+
+         tasks.length === 2
+         //No se como hacer que ignore el id
+         tasks(0) === Task(tasks(0).id, label, User(userNick), None)
+         tasks(1) === Task(tasks(1).id, label+label, User(userNick), None)
          tasks(0).id !== tasks(1).id
       }
 
