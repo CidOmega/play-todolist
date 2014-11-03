f081d9b7c8046d7567e85578ef2b42af1f7d9c5a
Edgar Perez Garcia <edgar-cid@hotmail.com>
2014-11-03 15:25:29 +0100

    mend

--- test/controllers/UserCategoriesSpec.scala
+++ test/controllers/UserCategoriesSpec.scala
@@ -32,7 +32,7 @@ class UserCategoriesSpec extends Specification with JsonMatchers {
 
    "Categorias de usuarios" should{
 
-      "POST /:owner/categories debe crear la categoria al usuario y devolver su json" in new WithApplication() {
+      "POST /:owner/categories debe crear la categoria al usuario y devolver su json y estado CREATED" in new WithApplication() {
          Category.exists(categories(0), userNicks(0)) must beFalse
 
          val home = route(FakeRequest(POST, "/" + userNicks(0) + "/categories").withFormUrlEncodedBody(("name", categories(0)))).get
@@ -45,6 +45,28 @@ class UserCategoriesSpec extends Specification with JsonMatchers {
          val jsonString = Json.stringify(contentAsJson(home))
 
          jsonString must /("name" -> categories(0))
+      }
+
+      "POST /:owner/categories debe devolver 404 si el usario no existe" in new WithApplication() {
+         Category.exists(categories(0), noUserNick) must beFalse
+
+         val home = route(FakeRequest(POST, "/" + noUserNick + "/categories").withFormUrlEncodedBody(("name", categories(0)))).get
+
+         Category.exists(categories(0), noUserNick) must beFalse
+
+         status(home) must equalTo(NOT_FOUND)
+         contentType(home) must beSome.which(_ == "text/plain")
+      }
+
+      "POST /:owner/categories debe devolver bad request si no se pasa el nombre de la categoria" in new WithApplication() {
+         Category.exists(categories(0), userNicks(0)) must beFalse
+
+         val home = route(FakeRequest(POST, "/" + userNicks(0) + "/categories").withFormUrlEncodedBody(("nombre", categories(0)))).get
+
+         Category.exists(categories(0), userNicks(0)) must beFalse
+
+         status(home) must equalTo(BAD_REQUEST)
+         contentType(home) must beSome.which(_ == "text/plain")
       }
 
    }
